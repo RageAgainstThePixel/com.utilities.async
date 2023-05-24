b8af8952239f7ca5dabfa0d8dfc01dda63995412
Stephen Hodgson <hodgson.designs@gmail.com>
2023-05-24 13:53:06 -0400

    com.utilities.async 1.2.4 (#6)
    - Added support for Addressables `AsyncOperationHandle<T>.GetAwaiter()`

--- Utilities.Async/Packages/com.utilities.async/Runtime/AssemblyInfo.cs
+++ Utilities.Async/Packages/com.utilities.async/Runtime/AssemblyInfo.cs
@@ -1,1 +1,5 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Utilities.Async.Addressables")]
