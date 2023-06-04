87e7edc946af0c873a6e558200d657baf73e4d1a
Stephen Hodgson <hodgson.designs@gmail.com>
2023-06-03 20:09:44 -0400

    com.utilities.async 2.0.0
    - Added WebGL support

--- Utilities.Async/Packages/com.utilities.async/Tests/TestFixture_01.cs
+++ Utilities.Async/Packages/com.utilities.async/Tests/TestFixture_01.cs
@@ -25,47 +25,43 @@ namespace Utilities.Async.Tests
         [Test]
         public async Task Test_02_Async()
         {
-            try
-            {
-                var stopwatch = Stopwatch.StartNew();
+            Debug.Log($"{nameof(Test_02_Async)} starting...");
+            var stopwatch = Stopwatch.StartNew();
 
-                // always encapsulate try/catch around
-                // async methods called from unity events
-                // this is a long running task that returns to main thread
-                await MyFunctionAsync().ConfigureAwait(true);
-                Debug.Log($"{nameof(MyFunctionAsync)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
-                Assert.IsTrue(SyncContextUtility.IsMainThread);
+            // always encapsulate try/catch around
+            // async methods called from unity events
+            // this is a long running task that returns to main thread
+            await MyFunctionAsync().ConfigureAwait(true);
+            Debug.Log($"{nameof(MyFunctionAsync)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
+            Assert.IsTrue(SyncContextUtility.IsMainThread);
 
-                // A long running task that ends up on a background thread
-                await MyFunctionAsync().ConfigureAwait(false);
-                Debug.Log($"{nameof(MyFunctionAsync)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
-                Assert.IsFalse(SyncContextUtility.IsMainThread);
+            // A long running task that ends up on a background thread
+            await MyFunctionAsync().ConfigureAwait(false);
+            Debug.Log($"{nameof(MyFunctionAsync)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
+            Assert.IsFalse(SyncContextUtility.IsMainThread);
 
-                // Get back to the main unity thread
-                await Awaiters.UnityMainThread;
-                Debug.Log($"{nameof(UnityMainThread)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
-                Assert.IsTrue(SyncContextUtility.IsMainThread);
+            // Get back to the main unity thread
+            await Awaiters.UnityMainThread;
+            Debug.Log($"{nameof(UnityMainThread)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
+            Assert.IsTrue(SyncContextUtility.IsMainThread);
 
-                // switch to background thread to do a long
-                // running process on background thread
-                await Awaiters.BackgroundThread;
-                Debug.Log($"{nameof(BackgroundThread)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
-                Assert.IsFalse(SyncContextUtility.IsMainThread);
+            // switch to background thread to do a long
+            // running process on background thread
+            await Awaiters.BackgroundThread;
+            Debug.Log($"{nameof(BackgroundThread)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
+            Assert.IsFalse(SyncContextUtility.IsMainThread);
 
-                // await on IEnumerator functions as well
-                // for backwards compatibility or older code
-                await MyEnumerableFunction();
-                Debug.Log($"{nameof(MyEnumerableFunction)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
-            }
-            catch (Exception e)
-            {
-                Debug.LogError(e);
-            }
+            // await on IEnumerator functions as well
+            // for backwards compatibility or older code
+            await MyEnumerableFunction();
+            Debug.Log($"{nameof(MyEnumerableFunction)} | {nameof(SyncContextUtility.IsMainThread)}? {SyncContextUtility.IsMainThread} | {stopwatch.ElapsedMilliseconds}");
+
+            Debug.Log($"{nameof(Test_02_Async)} Complete!");
         }
 
         private async Task MyFunctionAsync()
         {
-            await Task.Delay(1000);
+            await Task.Delay(1000).ConfigureAwait(false);
         }
 
         private IEnumerator MyEnumerableFunction()
