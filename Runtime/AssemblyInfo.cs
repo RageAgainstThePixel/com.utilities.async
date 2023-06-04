668684cd1a9a29b594713e798bf99424c063ca38
Stephen Hodgson <hodgson.designs@gmail.com>
2023-06-03 20:55:40 -0400

    com.utilities.async 2.0.0 (#8)
    - Added WebGL support
    - Special thanks to @VolodymyrBS for https://github.com/VolodymyrBS/WebGLThreadingPatcher

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Runtime.CompilerServices;
+using UnityEngine.Scripting;
 
+[assembly: AlwaysLinkAssembly]
 [assembly: InternalsVisibleTo("Utilities.Async.Addressables")]
