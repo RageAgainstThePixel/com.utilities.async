4a0bc244a6157f54bf9039a2b5294dd8d5f38943
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-04-10 21:57:04 -0400

    com.utilities.async 2.1.4 (#14)
    - added additional task cancellation token support extensions

--- Utilities.Async/Packages/com.utilities.async/Runtime/Async/AwaiterExtensions.cs
+++ Utilities.Async/Packages/com.utilities.async/Runtime/Async/AwaiterExtensions.cs
@@ -25,6 +25,7 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Scripting;
@@ -114,6 +115,54 @@ namespace Utilities.Async
         /// <param name="asyncFunc"><see cref="Func{TResult}"/> callback.</param>
         public static T RunSynchronously<T>(Func<Task<T>> asyncFunc)
             => asyncFunc.Invoke().Result;
+
+        /// <summary>
+        /// Runs <see cref="Task"/> with <see cref="CancellationToken"/>.
+        /// </summary>
+        /// <param name="task">The <see cref="Task"/> to run.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <exception cref="OperationCanceledException"></exception>
+        public static async Task WithCancellation(this Task task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            await using (cancellationToken.Register(state => ((TaskCompletionSource<object>)state).TrySetResult(null), tcs))
+            {
+                var resultTask = await Task.WhenAny(task, tcs.Task);
+
+                if (resultTask == tcs.Task)
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+
+                await task;
+            }
+        }
+
+        /// <summary>
+        /// Runs <see cref="Task{T}"/> with <see cref="CancellationToken"/>.
+        /// </summary>
+        /// <typeparam name="T">Task return type.</typeparam>
+        /// <param name="task">The <see cref="Task{T}"/> to run.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <exception cref="OperationCanceledException"></exception>
+        /// <returns><see cref="Task{T}"/> result.</returns>
+        public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            await using (cancellationToken.Register(state => ((TaskCompletionSource<object>)state).TrySetResult(null), tcs))
+            {
+                var resultTask = await Task.WhenAny(task, tcs.Task);
+
+                if (resultTask == tcs.Task)
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+
+                return await task;
+            }
+        }
 
         public static SimpleCoroutineAwaiter GetAwaiter(this UnityMainThread instruction)
             => GetAwaiterReturnVoid(instruction);
